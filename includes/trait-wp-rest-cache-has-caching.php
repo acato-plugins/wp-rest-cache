5657261422035a44e1f6799ced8ed68ee3cda38c
Corné Guijt <corne@wgpc.nl>
2018-10-22 15:26:19 +0200

    Fix local change

--- includes/trait-wp-rest-cache-has-caching.php
+++ includes/trait-wp-rest-cache-has-caching.php
@@ -1,57 +1,82 @@
 <?php
 
+/**
+ * Trait WP_Rest_Cache_Has_Caching
+ */
 trait WP_Rest_Cache_Has_Caching
 {
     /**
      * Prepares a single post output for response.
      *
-     * @param WP_Post $post Post object.
+     * @param WP_Post|WP_Term $item Post object.
      * @param WP_REST_Request $request Request object.
      * @return WP_REST_Response Response object.
      */
-    public function prepare_item_for_response($post, $request)
+    public function prepare_item_for_response($item, $request)
     {
-        $key = $this->transient_key($post);
+        $key = $this->transient_key($item);
 
         if (($value = get_transient($key)) == false) {
-            $value = $this->get_data($post, $request);
+            $value = $this->get_data($item, $request);
             set_transient($key, $value, WP_Rest_Cache::get_timeout());
         }
 
         return $value;
     }
 
-    public function get_data($post, $request)
+    /**
+     * @param WP_Post|WP_Term $item
+     * @param WP_REST_Request $request
+     * @return WP_REST_Response
+     */
+    public function get_data($item, $request)
     {
-        return parent::prepare_item_for_response($post, $request);
+        return parent::prepare_item_for_response($item, $request);
     }
 
-    public function update_item_cache(WP_Post $post)
+    /**
+     * @param WP_Term|WP_Post $item
+     */
+    public function update_item_cache($item)
     {
-        $data = $this->get_data($post, new WP_REST_Request());
+        $data = $this->get_data($item, new WP_REST_Request());
+        if(method_exists($this, 'enrich_data')){
+            $data = $this->enrich_data($item, $data);
+        }
 
-        set_transient($this->transient_key($post), $data, WP_Rest_Cache::get_timeout());
+        set_transient($this->transient_key($item), $data, WP_Rest_Cache::get_timeout());
     }
 
-    public function delete_item_cache(WP_Post $post)
+    /**
+     * @param WP_Term|WP_Post $item
+     */
+    public function delete_item_cache($item)
     {
-        delete_transient($this->transient_key($post));
+        delete_transient($this->transient_key($item));
     }
 
+    /**
+     * @param WP_Term|WP_Post $post
+     * @return string
+     */
     protected function transient_key($post)
     {
-        if(is_object($post)){
-            if($post instanceof WP_Post){
-                $id = $post->ID;
-            } elseif( $post instanceof WP_Term) {
-                $id = $post->term_id;
-            } else {
-                $id = $post;
-            }
-        } else {
-            $id = $post;
+        return WP_Rest_Cache::transient_key($this->get_id($post));
+    }
+
+    /**
+     * @param WP_Post|WP_Term $post
+     * @return int|string
+     */
+    protected function get_id($post)
+    {
+        switch(get_class($post)){
+            case WP_Post::class:
+                return $post->ID;
+            case WP_Term::class:
+                return 'taxonomy_' . $post->term_id;
+            default:
+                return $post;
         }
-        $id = $post instanceof WP_Post ? $post->ID : $post;
-        return WP_Rest_Cache::transient_key($id);
     }
 }
