f80c828f942443f7176573b58f6b0e9d98c08ef0
Corné Guijt <corne@wgpc.nl>
2018-10-22 15:17:30 +0200

    Fix local change

--- includes/class-wp-rest-cache-term-controller.php
+++ includes/class-wp-rest-cache-term-controller.php
@@ -2,45 +2,18 @@
 
 class WP_Rest_Cache_Term_Controller extends WP_REST_Terms_Controller
 {
-    /**
-     * Prepares a single post output for response.
-     *
-     * @param WP_Term $term Term object.
-     * @param WP_REST_Request $request Request object.
-     * @return WP_REST_Response Response object.
-     */
-    public function prepare_item_for_response($term, $request)
-    {
-        $key = $this->transient_key($term);
+    use WP_Rest_Cache_Has_Caching;
 
-        if (($value = get_transient($key)) == false) {
-            $value = $this->get_data($term, $request);
-            set_transient($key, $value, WP_Rest_Cache::get_timeout());
+    public function enrich_data(WP_Term $term, WP_REST_Response $response)
+    {
+        if(!class_exists('ACF_To_REST_API_ACF_API')){
+            return $response;
         }
 
-        return $value;
-    }
+        $acfController = new ACF_To_REST_API_ACF_API($term->name, 'ACF_To_REST_API_Terms_Controller');
+        $fields = $acfController->get_fields($term->term_id);
 
-    public function get_data($term, $request)
-    {
-        return parent::prepare_item_for_response($term, $request);
-    }
-
-    public function update_item_cache($term)
-    {
-        $data = $this->get_data($term, new WP_REST_Request());
-
-        set_transient($this->transient_key($term), $data, WP_Rest_Cache::get_timeout());
-    }
-
-    public function delete_item_cache($term)
-    {
-        delete_transient($this->transient_key($term));
-    }
-
-    private function transient_key($term)
-    {
-        $id = $term instanceof WP_Term ? $term->term_id : $term;
-        return WP_Rest_Cache::transient_key('taxonomy_' . $id);
+        $response->data['acf'] = $fields['acf'];
+        return $response;
     }
 }
