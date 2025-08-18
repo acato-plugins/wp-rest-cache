4edc61a9035080bf5c20cae20f3b381d80e63804
Richard Korthuis <richardkorthuis@acato.nl>
2025-08-18 11:41:04 +0200

    Version update ⬆️

--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -55,7 +55,7 @@ class Plugin {
 	 */
 	public function __construct() {
 		$this->plugin_name = 'wp-rest-cache';
-		$this->version     = '2025.1.4';
+		$this->version     = '2025.1.5';
 
 		$this->set_locale();
 		$this->define_admin_hooks();
